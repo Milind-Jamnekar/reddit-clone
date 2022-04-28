e66c6d5ea7419e7a6b728be860f22afe9bfa51e7
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-04-28 15:18:33 +0530

    Error list updated

--- src/pages/firebase/firebaseErrors.ts
+++ src/pages/firebase/firebaseErrors.ts
@@ -4,5 +4,8 @@ export const firebaseErrors = {
   "Firebase: Error (auth/email-already-in-use).":
     "Email account already exists ",
   "Firebase: Error (auth/user-not-found).": "Invalid email or password",
-  "Firebase: Error (auth/wrong-password).": "Wrong ",
+  "Firebase: Error (auth/wrong-password).": "Wrong password",
+  "Firebase: Error (auth/operation-not-allowed).":
+    "Firebase not allowing sign in. Please contact to developer",
+  "Firebase: Error (auth/popup-closed-by-user).": "Popup was closed by user",
 };
