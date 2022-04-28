0c8ac5c4ec60eba843fe9e344fc45e61d1a7430d
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-04-28 11:25:05 +0530

    Error mapping

---
+++ src/pages/firebase/firebaseErrors.ts
@@ -0,0 +1,8 @@
+export const firebaseErrors = {
+  "Firebase: Password should be at least 6 characters (auth/weak-password).":
+    "Password should be at least 6 characters",
+  "Firebase: Error (auth/email-already-in-use).":
+    "Email account already exists ",
+  "Firebase: Error (auth/user-not-found).": "Invalid email or password",
+  "Firebase: Error (auth/wrong-password).": "Wrong ",
+};
