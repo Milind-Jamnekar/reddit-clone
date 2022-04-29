3b26e591abd74df6f1dae0eb61d3e3c011285d0d
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-04-29 15:49:28 +0530

    changing right content according to user

--- src/pages/components/Navbar/RightContent/RightContent.tsx
+++ src/pages/components/Navbar/RightContent/RightContent.tsx
@@ -1,24 +1,22 @@
 import { Button, Flex } from "@chakra-ui/react";
-import { signOut } from "firebase/auth";
+import { signOut, User } from "firebase/auth";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../../firebase/clientApp";
 import AuthModal from "../../Modal/Auth/AuthModal";
 import AuthButtons from "./AuthButtos";
+import { Icons } from "./Icons";
 
 type RightContentProps = {
-  user: any;
+  user?: User | null;
 };
 
 const RightContent: React.FC<RightContentProps> = ({ user }) => {
   return (
     <Flex align="center">
       <AuthModal />
-      {user ? (
-        <Button onClick={() => signOut(auth)}>Sign Out</Button>
-      ) : (
-        <AuthButtons />
-      )}
+      {user ? <Icons /> : <AuthButtons />}
+      {/* <Menu /> */}
     </Flex>
   );
 };
