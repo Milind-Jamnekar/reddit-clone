420ae4952a9504c8eb0dff3a48cf1f0e94ac8995
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-04-29 17:06:43 +0530

    Modifying some layout

--- src/pages/components/Navbar/RightContent/AuthButtos.tsx
+++ src/pages/components/Navbar/RightContent/AuthButtos.tsx
@@ -6,13 +6,12 @@ import { authModalState } from "../../../atoms/authModalAtom";
 const AuthButtons: React.FC = () => {
   const setModalState = useSetRecoilState(authModalState);
   return (
-    <>
+    <Flex gap={2} mx={2}>
       <Button
         variant="outline"
         height="28px"
         display={{ base: "none", sm: "flex" }}
         width={{ base: "70px", md: "110px" }}
-        ms={2}
         onClick={() => setModalState({ open: true, view: "login" })}
       >
         Login
@@ -21,12 +20,11 @@ const AuthButtons: React.FC = () => {
         height="28px"
         display={{ base: "none", sm: "flex" }}
         width={{ base: "70px", md: "110px" }}
-        ms={2}
         onClick={() => setModalState({ open: true, view: "signup" })}
       >
         Signup
       </Button>
-    </>
+    </Flex>
   );
 };
 
