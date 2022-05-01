1a803211977814192c3239e0f241480f617da36f
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-05-01 20:52:06 +0530

    Not Found page component

---
+++ src/components/Modal/Community/NotFound.tsx
@@ -0,0 +1,18 @@
+import { Button, Flex, Text } from "@chakra-ui/react";
+import Link from "next/link";
+import React from "react";
+
+export const NotFound: React.FC = () => {
+  return (
+    <Flex direction="column" justify={"center"} align="center" minHeight="60vh">
+      <Text fontSize="4xl">
+        Sorry, This community does not exists or has been banned! 😶
+      </Text>
+      <Link href="/">
+        <Button mt="4" size="lg">
+          GO Home
+        </Button>
+      </Link>
+    </Flex>
+  );
+};
