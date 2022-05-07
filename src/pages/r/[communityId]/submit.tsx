c6804108f5e259690ad6c2b57ef9655ba4e1b63b
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-05-07 19:48:30 +0530

    passing auth user form thie file

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,16 +1,19 @@
 import { Box, Text } from "@chakra-ui/react";
 import { NextPage } from "next";
+import { useAuthState } from "react-firebase-hooks/auth";
 import PageContent from "../../../components/Layout/PageContent";
 import NewPostForm from "../../../components/Posts/NewPostForm";
+import { auth } from "../../../firebase/clientApp";
 
 const SubmitPage: NextPage = () => {
+  const [user] = useAuthState(auth);
   return (
     <PageContent>
       <>
         <Box p="14px 0px" borderBottom="1px solid " borderColor="white">
           <Text>Create post</Text>
         </Box>
-        <NewPostForm />
+        {user && <NewPostForm user={user} />}
       </>
       <></>
       {/* <>About </> */}
