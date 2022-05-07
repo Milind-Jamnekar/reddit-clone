c7ac1182b64b2155489ac611b93e39937af6d932
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-05-07 19:47:20 +0530

    Post typing defination and created atom for it

---
+++ src/atoms/postsAtom.ts
@@ -0,0 +1,31 @@
+import { Timestamp } from "firebase/firestore";
+import { atom } from "recoil";
+
+export type Post = {
+  id: string;
+  communityId: string;
+  creatorId: string;
+  creatorDisplayName: string;
+  title: string;
+  body: string;
+  numberOfComments: number;
+  voteStatus: number;
+  imageURL?: string;
+  communityImageURL?: string;
+  createdAt: Timestamp;
+};
+
+interface PostState {
+  selectedPost: Post | null;
+  posts: Post[];
+}
+
+const defaultPostState: PostState = {
+  selectedPost: null,
+  posts: [],
+};
+
+export const postState = atom<PostState>({
+  key: "postState",
+  default: defaultPostState,
+});
