fce0d106796a2eb53c276235921457e636338fdb
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-05-07 19:46:57 +0530

    naming change

--- src/atoms/communitiesAtom.ts
+++ src/atoms/communitiesAtom.ts
@@ -13,7 +13,7 @@ export interface Community {
 export interface CommunitySnippet {
   communityId: string;
   isMod: boolean;
-  profileImageURL: string;
+  communityImageURL: string;
 }
 
 interface CommunityState {
