a887ea57d201c6d73e0095ea35b9d21f98f44732
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-05-08 17:40:06 +0530

    Post UI

---
+++ src/components/Posts/Posts.tsx
@@ -0,0 +1,76 @@
+import { Skeleton, Stack } from "@chakra-ui/react";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Community } from "../../atoms/communitiesAtom";
+import { Post } from "../../atoms/postsAtom";
+import { auth, firestore } from "../../firebase/clientApp";
+import usePostsData from "../../hooks/usePostsData";
+import Loading from "./Loading";
+import PostItem from "./PostItem";
+
+interface IPostsProps {
+  communityData: Community;
+}
+
+const Posts: React.FunctionComponent<IPostsProps> = ({ communityData }) => {
+  const [user] = useAuthState(auth);
+  const [loading, setLoading] = useState(false);
+  const {
+    postStateValue,
+    setPostStateValue,
+    onVote,
+    onSelectPost,
+    onDeletePost,
+  } = usePostsData();
+
+  const getPosts = async () => {
+    setLoading(true);
+    try {
+      const postsQuery = query(
+        collection(firestore, "posts"),
+        where("communityId", "==", communityData.id),
+        orderBy("createdAt", "desc")
+      );
+      const postsSnapshot = await getDocs(postsQuery);
+      const posts = postsSnapshot.docs.map((post) => ({
+        id: post.id,
+        ...post.data(),
+      }));
+      setPostStateValue((prev) => ({
+        ...prev,
+        posts: posts as Post[],
+      }));
+      console.log(posts);
+    } catch (error: any) {
+      console.log("getPosts error", error.message);
+    }
+    setLoading(false);
+  };
+  useEffect(() => {
+    getPosts();
+  }, []);
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <Stack>
+          {postStateValue.posts.map((post) => (
+            <PostItem
+              key={post.id}
+              post={post}
+              userIsCreator={user?.uid === post.creatorId}
+              userVoteStatus={undefined}
+              onVote={onVote}
+              onSelectPost={onSelectPost}
+              onDeletePost={onDeletePost}
+            />
+          ))}
+        </Stack>
+      )}
+    </>
+  );
+};
+
+export default Posts;
