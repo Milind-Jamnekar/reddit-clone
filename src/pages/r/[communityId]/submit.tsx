cd45be19775cbba1bd19e4ef3b4842bb76872a42
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-05-10 15:46:05 +0530

    submit page subscribe to communityStateValue and user

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,12 +1,16 @@
 import { Box, Text } from "@chakra-ui/react";
 import { NextPage } from "next";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useRecoilValue } from "recoil";
+import { CommunityState } from "../../../atoms/communitiesAtom";
 import PageContent from "../../../components/Layout/PageContent";
 import NewPostForm from "../../../components/Posts/NewPostForm";
 import { auth } from "../../../firebase/clientApp";
 
 const SubmitPage: NextPage = () => {
+  const communityStateValue = useRecoilValue(CommunityState);
   const [user] = useAuthState(auth);
+
   return (
     <PageContent>
       <>
