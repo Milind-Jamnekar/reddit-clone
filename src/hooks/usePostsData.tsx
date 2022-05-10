a94fe10c1d96a5e289c48b5e49d8da27eeda031b
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-05-10 15:43:40 +0530

    deleting post functionality

--- src/hooks/usePostsData.tsx
+++ src/hooks/usePostsData.tsx
@@ -1,7 +1,10 @@
+import { deleteDoc, doc } from "firebase/firestore";
+import { deleteObject, ref } from "firebase/storage";
 import React from "react";
 
 import { useRecoilState } from "recoil";
-import { postState } from "../atoms/postsAtom";
+import { Post, postState } from "../atoms/postsAtom";
+import { firestore, storage } from "../firebase/clientApp";
 
 const usePostsData = () => {
   const [postStateValue, setPostStateValue] = useRecoilState(postState);
@@ -10,7 +13,28 @@ const usePostsData = () => {
 
   const onSelectPost = () => {};
 
-  const onDeletePost = async () => {};
+  const onDeletePost = async (post: Post): Promise<boolean> => {
+    try {
+      if (post.imageURL) {
+        const imageRef = ref(storage, `posts/${post.id}/image`);
+        await deleteObject(imageRef);
+      }
+
+      // delete post from db
+      const postDocRef = doc(firestore, "posts", post.id);
+      await deleteDoc(postDocRef);
+
+      // delete from poststate atom
+      setPostStateValue((prev) => ({
+        ...prev,
+        posts: prev.posts.filter((item) => item.id !== post.id),
+      }));
+      return true;
+    } catch (error: any) {
+      return false;
+    }
+    //Checks if image exist if yes delete it from db
+  };
 
   return {
     postStateValue,
