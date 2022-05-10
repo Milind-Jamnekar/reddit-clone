b2c0bc528b70bd78699a4adf1872b97e702695bd
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-05-10 15:39:59 +0530

    current Community property for submit component

--- src/atoms/communitiesAtom.ts
+++ src/atoms/communitiesAtom.ts
@@ -18,6 +18,7 @@ export interface CommunitySnippet {
 
 interface CommunityState {
   mySnippets: CommunitySnippet[];
+  currentCommunity?: Community;
 }
 
 const defaultCommunityState: CommunityState = {
