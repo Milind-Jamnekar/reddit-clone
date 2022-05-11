c63588971718f198916591823c32995abed9b651
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-05-11 12:49:22 +0530

    postvote type defination

--- src/atoms/postsAtom.ts
+++ src/atoms/postsAtom.ts
@@ -15,14 +15,23 @@ export type Post = {
   createdAt: Timestamp;
 };
 
-interface PostState {
+export type PostVote = {
+  id: string;
+  postId: string;
+  communityId: string;
+  voteValue: number;
+};
+
+export interface PostState {
   selectedPost: Post | null;
   posts: Post[];
+  postVotes: PostVote[];
 }
 
 const defaultPostState: PostState = {
   selectedPost: null,
   posts: [],
+  postVotes: [],
 };
 
 export const postState = atom<PostState>({
