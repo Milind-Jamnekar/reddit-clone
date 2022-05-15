3fdd7856230dbad759928ac7ad1f4b2db4f5d086
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-05-15 22:29:45 +0530

    post page is now available for direct link like sharing that link will be possible

--- src/pages/r/[communityId]/comments/[pid].tsx
+++ src/pages/r/[communityId]/comments/[pid].tsx
@@ -1,14 +1,22 @@
+import { User } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
 import { NextPage } from "next";
-import * as React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { Post } from "../../../../atoms/postsAtom";
+import About from "../../../../components/Communtiy/About";
 import PageContent from "../../../../components/Layout/PageContent";
+import Comments from "../../../../components/Posts/Comments/Comments";
 import PostItem from "../../../../components/Posts/PostItem";
-import { auth } from "../../../../firebase/clientApp";
+import { auth, firestore } from "../../../../firebase/clientApp";
+import { useCommunityData } from "../../../../hooks/useCommunityData";
 import usePostsData from "../../../../hooks/usePostsData";
 
 interface IPostPageProps {}
 
 const PostPage: NextPage = (props) => {
+  const router = useRouter();
   const [user] = useAuthState(auth);
   const {
     setPostStateValue,
@@ -17,6 +25,31 @@ const PostPage: NextPage = (props) => {
     onSelectPost,
     onDeletePost,
   } = usePostsData();
+
+  const { communityStateValue } = useCommunityData();
+
+  const fetchPost = async (pid: string) => {
+    try {
+      const postDocRef = doc(firestore, "posts", pid);
+      const postDoc = await getDoc(postDocRef);
+
+      setPostStateValue((prev) => ({
+        ...prev,
+        selectedPost: { id: postDoc.id, ...postDoc.data() } as Post,
+      }));
+    } catch (error: any) {
+      console.log("handle error in pid", error.message);
+    }
+  };
+
+  useEffect(() => {
+    const pid = router.query.pid;
+
+    if (pid && !communityStateValue.currentCommunity) {
+      fetchPost(pid as string);
+    }
+  }, [router.query, communityStateValue.currentCommunity]);
+
   return (
     <PageContent>
       <>
@@ -33,8 +66,17 @@ const PostPage: NextPage = (props) => {
             userIsCreator={user?.uid === postStateValue.selectedPost?.creatorId}
           />
         )}
+        <Comments
+          user={user as User}
+          selectedPost={postStateValue.selectedPost}
+          communityId={communityStateValue.currentCommunity?.id as string}
+        />
       </>
-      <></>
+      <>
+        {communityStateValue.currentCommunity && (
+          <About communityData={communityStateValue.currentCommunity} />
+        )}
+      </>
     </PageContent>
   );
 };
