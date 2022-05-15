cc7ad1f0a514f3492533ecc7375093f31cc5e2bc
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-05-15 22:28:26 +0530

    get community data whenever route change and state value of community changed

--- src/hooks/useCommunityData.tsx
+++ src/hooks/useCommunityData.tsx
@@ -6,6 +6,7 @@ import {
   increment,
   writeBatch,
 } from "firebase/firestore";
+import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useRecoilState, useSetRecoilState } from "recoil";
@@ -18,6 +19,7 @@ import {
 import { auth, firestore } from "../firebase/clientApp";
 
 export const useCommunityData = () => {
+  const router = useRouter();
   const [user] = useAuthState(auth);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
@@ -139,6 +141,20 @@ export const useCommunityData = () => {
     setLoading(false);
   };
 
+  const getCommunityData = async (communityId: string) => {
+    try {
+      const communityDocRef = doc(firestore, "communities", communityId);
+      const communityDoc = await getDoc(communityDocRef);
+      setCommunityStateValue((prev) => ({
+        ...prev,
+        currentCommunity: {
+          id: communityDoc.id,
+          ...communityDoc.data(),
+        } as Community,
+      }));
+    } catch (error: any) {}
+  };
+
   useEffect(() => {
     if (!user) {
       setCommunityStateValue((prev) => ({
@@ -149,6 +165,14 @@ export const useCommunityData = () => {
     }
     getMySnippets();
   }, [user]);
+
+  useEffect(() => {
+    const { communityId } = router.query;
+
+    if (communityId && !communityStateValue.currentCommunity) {
+      getCommunityData(communityId as string);
+    }
+  }, [router.query, communityStateValue.currentCommunity]);
 
   return {
     communityStateValue,
