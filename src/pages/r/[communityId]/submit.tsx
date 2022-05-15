9ccdae8aa396311d3d9faee1cd0c592816b7df57
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-05-15 22:28:49 +0530

    about is integrate to submit

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -3,24 +3,31 @@ import { NextPage } from "next";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useRecoilValue } from "recoil";
 import { CommunityState } from "../../../atoms/communitiesAtom";
+import About from "../../../components/Communtiy/About";
 import PageContent from "../../../components/Layout/PageContent";
 import NewPostForm from "../../../components/Posts/NewPostForm";
 import { auth } from "../../../firebase/clientApp";
+import { useCommunityData } from "../../../hooks/useCommunityData";
 
 const SubmitPage: NextPage = () => {
-  const communityStateValue = useRecoilValue(CommunityState);
+  const { communityStateValue } = useCommunityData();
   const [user] = useAuthState(auth);
 
   return (
     <PageContent>
       <>
-        <Box p="14px 0px" borderBottom="1px solid " borderColor="white">
-          <Text>Create post</Text>
+        <Box p="0px 0px" borderBottom="1px solid " borderColor="white">
+          <Text fontSize="xl" fontWeight="bold">
+            Create post
+          </Text>
         </Box>
         {user && <NewPostForm user={user} />}
       </>
-      <></>
-      {/* <>About </> */}
+      <>
+        {communityStateValue.currentCommunity && (
+          <About communityData={communityStateValue.currentCommunity} />
+        )}
+      </>
     </PageContent>
   );
 };
