e510d2b0e14340edacdd7d41c1d5e5827688d25e
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-05-17 22:53:35 +0530

    displaying all community user joined

---
+++ src/components/Navbar/Directory/MenuListItems.tsx
@@ -0,0 +1,45 @@
+import { Flex, Icon, Image, MenuItem } from "@chakra-ui/react";
+import React from "react";
+import { IconType } from "react-icons";
+
+interface IMenuListItemProps {
+  displayText: string;
+  link: string;
+  icon: IconType;
+  iconColor?: string;
+  imageURL?: string;
+}
+
+const MenuListItem: React.FC<IMenuListItemProps> = ({
+  imageURL,
+  iconColor,
+  link,
+  icon,
+  displayText,
+}) => {
+  return (
+    <MenuItem
+      width="100%"
+      fontSize="10pt"
+      _hover={{ bg: "gray.100" }}
+      onClick={() => {}}
+    >
+      <Flex align="center">
+        {imageURL ? (
+          <Image
+            src={imageURL}
+            borderRadius="full"
+            boxSize="35px"
+            mr={2}
+            alt={`${displayText} profile image`}
+          />
+        ) : (
+          <Icon as={icon} fontSize={35} mr={2} color={iconColor} />
+        )}
+        {displayText}
+      </Flex>
+    </MenuItem>
+  );
+};
+
+export default MenuListItem;
