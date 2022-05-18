2071bf60acf5096938bf52ab5321df17e98c4cc6
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-05-18 21:34:29 +0530

    useDirectory hook for display communities on menuItem

---
+++ src/atoms/directoryMenuItem.ts
@@ -0,0 +1,33 @@
+import { IconType } from "react-icons";
+import { TiHome } from "react-icons/ti";
+import { atom } from "recoil";
+
+export type DirectoryMenuItem = {
+  displayText: string;
+  link: string;
+  icon: IconType;
+  iconColor?: string;
+  imageURL?: string;
+};
+
+interface DirectoryMenuState {
+  isOpen: boolean;
+  selectedMenuItem: DirectoryMenuItem;
+}
+
+const defaultMenuItem: DirectoryMenuItem = {
+  displayText: "home",
+  link: "/",
+  icon: TiHome,
+  iconColor: "black",
+};
+
+const defaultMenuState: DirectoryMenuState = {
+  isOpen: false,
+  selectedMenuItem: defaultMenuItem,
+};
+
+export const directoryMenuState = atom<DirectoryMenuState>({
+  key: "directoryMenuState",
+  default: defaultMenuState,
+});
