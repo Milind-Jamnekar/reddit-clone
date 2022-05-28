78e0f7b6d387a308a135a3b5537e62b6c72e0689
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-05-28 23:52:05 +0530

    profile image on directory itme

--- src/components/Navbar/Directory/Directory.tsx
+++ src/components/Navbar/Directory/Directory.tsx
@@ -7,6 +7,7 @@ import {
   MenuButton,
   MenuList,
   Text,
+  Image,
 } from "@chakra-ui/react";
 import React from "react";
 import { TiHome } from "react-icons/ti";
@@ -30,8 +31,24 @@ export const Directory: React.FC = () => {
           width={{ base: "auto", lg: "200px" }}
         >
           <Flex align="center">
-            <Icon as={TiHome} fontSize={24} mr={{ base: 1, md: 2 }}></Icon>
-            <Text display={{ base: "none", md: "unset" }}>Home</Text>
+            {directoryState.selectedMenuItem.imageURL ? (
+              <Image
+                src={directoryState.selectedMenuItem.imageURL}
+                borderRadius="full"
+                boxSize="24px"
+                mr={2}
+                alt={directoryState.selectedMenuItem.displayText + "image"}
+              />
+            ) : (
+              <Icon
+                as={directoryState.selectedMenuItem.icon}
+                fontSize={24}
+                mr={{ base: 1, md: 2 }}
+              ></Icon>
+            )}
+            <Text display={{ base: "none", md: "unset" }}>
+              {directoryState.selectedMenuItem.displayText}
+            </Text>
           </Flex>
           <ChevronDownIcon fontSize={23} />
         </HStack>
