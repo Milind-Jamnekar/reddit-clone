80b4f62b0c4d60f3582c7eabd2175906b849dc48
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-05-28 23:46:04 +0530

    toggleMenu

--- src/components/Communtiy/CreatePostLink.tsx
+++ src/components/Communtiy/CreatePostLink.tsx
@@ -5,12 +5,13 @@ import React from "react";
 import { BsLink45Deg } from "react-icons/bs";
 import { FaReddit } from "react-icons/fa";
 import { IoImageOutline } from "react-icons/io5";
-// import useDirectory from "../../hooks/useDirectory";
+import useDirectory from "../../hooks/useDirectory";
 
 type CreatePostProps = {};
 
 const CreatePostLink: React.FC<CreatePostProps> = () => {
   const router = useRouter();
+  const { toggleMenuOpen } = useDirectory();
   // const { toggleMenuOpen } = useDirectory();
   const onClick = () => {
     // Could check for user to open auth modal before redirecting to submit
@@ -22,7 +23,7 @@ const CreatePostLink: React.FC<CreatePostProps> = () => {
     // }
     router.push(`${communityId}/submit`);
     // Open directory menu to select community to post to
-    // toggleMenuOpen();
+    toggleMenuOpen();
   };
   return (
     <Flex
