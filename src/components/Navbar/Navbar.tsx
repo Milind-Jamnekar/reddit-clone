4951bb18cf7f783087174386e97fa703444d44a8
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-05-28 23:47:52 +0530

    navbar logo redirect to home with changing MenuItem Object

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -8,6 +8,7 @@ import SearchInput from "./SearchInput";
 
 const Navbar: React.FC = () => {
   const [user, loading, error] = useAuthState(auth);
+  const { onSelectMenuItem } = useDirectory();
 
   return (
     <Flex
