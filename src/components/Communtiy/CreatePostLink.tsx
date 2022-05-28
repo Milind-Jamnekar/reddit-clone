4e111eaff6b540774db2bd42f0adcd8cc3c1a9c3
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-05-28 23:49:01 +0530

    organize file

--- src/components/Communtiy/CreatePostLink.tsx
+++ src/components/Communtiy/CreatePostLink.tsx
@@ -1,5 +1,4 @@
 import { Flex, Icon, Input } from "@chakra-ui/react";
-import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 import { BsLink45Deg } from "react-icons/bs";
