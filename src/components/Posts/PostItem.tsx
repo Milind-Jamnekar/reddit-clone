3b16144c3b9f7491c016d5900268a96dd3d7ce1b
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-06-01 19:39:01 +0530

    improved version of kformatter

--- src/components/Posts/PostItem.tsx
+++ src/components/Posts/PostItem.tsx
@@ -82,9 +82,11 @@ const PostItem: React.FunctionComponent<IPostItemProps> = ({
 
   // 1000 karma convert to 1k
   function kFormatter(num: number) {
-    return Math.abs(num) > 999
-      ? Math.sign(num) * (Math.abs(num) / 1000).toFixed(1) + "k"
-      : Math.sign(num) * Math.abs(num);
+    if (num > 999) {
+      return (num / 1000).toFixed(1) + "k";
+    } else {
+      return num;
+    }
   }
 
   const variant = {
