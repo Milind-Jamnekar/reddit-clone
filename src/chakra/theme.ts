fbaf93cd0189e8fc5a4c63b5a2b2262b4cacc55a
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-06-07 09:02:49 +0530

    background color change to mode

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -1,9 +1,8 @@
 import { extendTheme } from "@chakra-ui/react";
-import { Button } from "./button";
-
 import "@fontsource/open-sans/300.css";
 import "@fontsource/open-sans/400.css";
 import "@fontsource/open-sans/700.css";
+import { Button } from "./button";
 
 export const theme = extendTheme({
   initialColorMode: "system",
@@ -15,9 +14,9 @@ export const theme = extendTheme({
     body: "Open Sans, sans-sarif",
   },
   styles: {
-    global: () => ({
+    global: (props: any) => ({
       body: {
-        bg: "gray.200",
+        bg: props.colorMode === "light" ? "gray.200" : "blackAlpha.700",
       },
     }),
   },
