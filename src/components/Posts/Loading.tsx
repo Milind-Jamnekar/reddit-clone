bfaafa630d21f2e5eb1bda485bc987087efcde27
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-06-09 16:53:10 +0530

    dark enabled

--- src/components/Posts/Loading.tsx
+++ src/components/Posts/Loading.tsx
@@ -3,12 +3,12 @@ import { Stack, Box, SkeletonText, Skeleton } from "@chakra-ui/react";
 const PostLoader = () => {
   return (
     <Stack spacing={6}>
-      <Box padding="10px 10px" boxShadow="lg" bg="white" borderRadius={4}>
+      <Box padding="10px 10px" boxShadow="xl" borderRadius={5}>
         <SkeletonText mt="4" noOfLines={1} width="40%" spacing="4" />
         <SkeletonText mt="4" noOfLines={4} spacing="4" />
         <Skeleton mt="4" height="200px" />
       </Box>
-      <Box padding="10px 10px" boxShadow="lg" bg="white" borderRadius={4}>
+      <Box padding="10px 10px" boxShadow="xl" borderRadius={5}>
         <SkeletonText mt="4" noOfLines={1} width="40%" spacing="4" />
         <SkeletonText mt="4" noOfLines={4} spacing="4" />
         <Skeleton mt="4" height="200px" />
